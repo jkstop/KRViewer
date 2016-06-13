2f69c69eeabb4370b01b5f64a31a621ffd05f4c9
Илья <jkstop@yandex.ru>
2016-06-13 22:24:06 +0300

    home commit

--- app/src/main/java/ru/jkstop/krviewer/MainActivity.java
+++ app/src/main/java/ru/jkstop/krviewer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.view.MenuInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -93,14 +95,28 @@ public class MainActivity extends AppCompatActivity
                     replaceFragment(JournalFragment.newInstance(), getString(R.string.menu_navigation_journal));
                 }
                 break;
-            case R.id.menu_navigation_settings:
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-                break;
             default:
                 break;
         }
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_main_server_status:
+                Toast.makeText(MainActivity.this, "SQL", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
